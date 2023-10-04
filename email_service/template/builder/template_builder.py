7fcdf6e50ca17c4c28f9abee0080a2457bc73d74
Ivrogo <er_jotx@hotmail.com>
2023-10-04 10:17:58 +0200

    Added template builder + test

--- email_service/template/builder/template_builder.py
+++ email_service/template/builder/template_builder.py
@@ -1,29 +1,24 @@
 class TemplateBuilder:
     def build_template(self, template_data, company_logo_path):
         # Suponemos que tendremos un logo dentro del proyecto (pondre un placeholder)
-        with open(company_logo_path, 'rb') as logo_file:
-            logo_content = logo_file.read()
+        local_url = 'file://' + company_logo_path
         
-        # Convertimos los bytes del logo en una cadena base64
-        import base64
-        logo_base64 = base64.b64encode(logo_content).decode('utf-8')
+        # Obtenemos los datos del template
+        name = template_data.get('name', '')
+        message = template_data.get('message', '')
+        
         
         # Definimos una plantilla HTML basica
         email_template = f"""
         <html>
             <head></head>
             <body>
-                <img src="data:image/png;base64,{logo_base64}" alt="Logo de la empresa">
-                <h1>Bienvenido, {{ name }}!</h1>
-                <p>{{ message }}</p>
+                <img src="{local_url}" alt="Logo de la empresa">
+                <h1>Bienvenido, {name}!</h1>
+                <p>{message}</p>
             </body>
         </html>
         """
-        
-        # Reemplazamos las variables de la plantilla con datos proporcionados (tiene que ser un diccionario de Python (key:value))
-        for key, value in template_data.items():
-            placeholder = "{{" + key + "}}"
-            email_template = email_template.replace(placeholder, str(value))
             
         return email_template
     
---
+++ email_service/template/builder/test/template_builder_test.py
@@ -0,0 +1,34 @@
+import unittest
+
+from email_service.template.builder.template_builder import TemplateBuilder
+
+class TestTemplateBuilder(unittest.TestCase):
+    def test_build_template(self):
+        # Datos de prueba
+        template_data = {
+            'name': 'Ivan Rodriguez',
+            'message': 'Hola, esto es un mensaje de prueba'
+        }
+        company_logo_path = 'email_service/template/x17.jpg'
+        
+        # Construimos la plantilla con el TemplateBuilder
+        template_builder = TemplateBuilder()
+        actual_template = template_builder.build_template(template_data, company_logo_path)
+        
+        expected_template = f"""
+        <html>
+            <head></head>
+            <body>
+                <img src="file://{company_logo_path}" alt="Logo de la empresa">
+                <h1>Bienvenido, {template_data['name']}!</h1>
+                <p>{template_data['message']}</p>
+            </body>
+        </html>
+        """
+        
+        # Comprobamos si la plantilla generada coincide con la plantilla esperada
+        self.assertEqual(actual_template.strip(), expected_template.strip())
+        
+        
+if __name__ == '__main__':
+    unittest.main()
